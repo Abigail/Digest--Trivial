c1f4bb8efb54cc3236bac490d2ba3cd23bf46125
Abigail <abigail@abigail.be>
2010-01-13 22:07:57 +0100

    Kwalitee test changed to an author only one

--- lib/Digest/Trivial.pm
+++ lib/Digest/Trivial.pm
@@ -9,7 +9,7 @@ use AutoLoader;
 
 our @ISA     = qw (Exporter);
 our @EXPORT  = qw (trivial_x trivial_s);
-our $VERSION = '2009112401';
+our $VERSION = '2010011301';
 
 require XSLoader;
 XSLoader::load ('Digest::Trivial', $VERSION);
